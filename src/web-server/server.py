8b02f06ee68b684dce24635928f75497a39eceb3
Rong Jiang <maikfangogoair@gmail.com>
2018-01-14 14:41:02 +0800

    nginx server

--- src/web-server/server.py
+++ src/web-server/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from motor import motor_control 
 from servo import camera_control
+from servo import ultrasonic_control
 from flask import request
 
 app = Flask(__name__)
@@ -63,7 +64,24 @@ def camera_reset():
     cc.reset()
     return "OK"
 
+
+@app.route("/back_uc_left")
+def back_uc_left():
+    back_uc.horizontal_move(direction = 1.0)
+    return "OK"
+ 
+@app.route("/back_uc_right")
+def back_uc_right():
+    back_uc.horizontal_move(direction = -1.0)
+    return "OK"
+
+@app.route("/back_uc_reset")
+def back_uc_reset():
+    back_uc.reset()
+    return "OK"
+
 if __name__ == "__main__":
     mc = motor_control.CONTROL(Frequency=300)
     cc = camera_control.CONTROL(STRIDE= 0.1)
+    back_uc = ultrasonic_control.CONTROL(STRIDE= 0.1, PIN=18)
     app.run(host='0.0.0.0', port=2000, debug=False)
--- src/web-server/servo/ultrasonic_control.py
+++ src/web-server/servo/ultrasonic_control.py
@@ -2,27 +2,21 @@ import RPi.GPIO as GPIO
 import time   
 
 class CONTROL:
-	def __init__(self, STRIDE= 0.01, NOMINAL=7.5, RANGE=3.5):
+	def __init__(self, STRIDE= 0.01, NOMINAL=7.5, RANGE=3.5, PIN=18):
 		self.STRIDE= STRIDE 
 		self.NOMINAL= NOMINAL 
 		self.RANGE = RANGE  
 		self.previous_hor_dc = self.NOMINAL
-		self.previous_ver_dc = self.NOMINAL
 
 		GPIO.setmode(GPIO.BCM)  
-		GPIO.setup(26, GPIO.OUT, initial=False)  
-		self.horizontal = GPIO.PWM(26,50)
+		GPIO.setup(PIN, GPIO.OUT, initial=False)  
+		self.horizontal = GPIO.PWM(PIN,50)
 		self.horizontal.start(self.NOMINAL)  
-		time.sleep(1)  
-
-		GPIO.setup(19, GPIO.OUT, initial=False)  
-		self.vertical = GPIO.PWM(19,50)
-		self.vertical.start(self.NOMINAL)  
 		time.sleep(1)  
 
 		self.MAX_DC = self.NOMINAL +  self.RANGE
 		self.MIN_DC = self.NOMINAL -  self.RANGE
-		print("init camera servo success")
+		print("init %s success" % PIN)
 
 	def horizontal_move(self,direction=1.0):
 	    dutycycle = self.previous_hor_dc + direction * self.RANGE * self.STRIDE
@@ -34,18 +28,6 @@ class CONTROL:
 	    self.horizontal.ChangeDutyCycle(dutycycle)  
 	    self.previous_hor_dc = dutycycle
 
-	def vertical_move(self,direction=1.0):
-	    dutycycle = self.previous_ver_dc + direction * self.RANGE * self.STRIDE
-	    print(dutycycle)
-	    if dutycycle > self.MAX_DC: 
-	    	dutycycle = self.MAX_DC
-	    elif dutycycle < self.MIN_DC:
-	    	dutycycle = self.MIN_DC
-	    self.vertical.ChangeDutyCycle(dutycycle)  
-	    self.previous_ver_dc = dutycycle
-
 	def reset(self):
-		self.vertical.ChangeDutyCycle(self.NOMINAL)
 		self.horizontal.ChangeDutyCycle(self.NOMINAL)  
 		self.previous_hor_dc = self.NOMINAL
-		self.previous_ver_dc = self.NOMINAL
